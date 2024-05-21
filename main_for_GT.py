aa4135892039d654251cd5e098d533b5cc013adb
masserep <pierre.masserey@unifr.ch>
2024-05-21 09:28:33 +0200

    fixed gazetracking (I hope)

--- main_for_GT.py
+++ main_for_GT.py
@@ -3,11 +3,23 @@ import speech_recognition as sr
 import threading
 from GazeTracking import example as gt
 
+pyautogui.FAILSAFE = False
+
+last_input = []
+recognizer = sr.Recognizer()
 
 def stop():
-    for i in last_input:
-        pyautogui.keyUp(i)
-    last_input.clear()
+    # Relâcher toutes les touches
+    pyautogui.keyUp('w')
+    pyautogui.keyUp('a')
+    pyautogui.keyUp('s')
+    pyautogui.keyUp('d')
+    pyautogui.keyUp('3')
+    pyautogui.keyUp('space')
+    
+    # Relâcher tous les boutons de la souris
+    pyautogui.mouseUp(button='left')
+    pyautogui.mouseUp(button='right')
 
 def go_forward():
     pyautogui.keyDown('w')
@@ -25,8 +37,8 @@ def go_right():
     pyautogui.keyDown('d')
     last_input.append('d')
 
-def slot_1():
-    pyautogui.keyDown('1')
+def slot_3():
+    pyautogui.keyDown('3')
 
 def jump():
     pyautogui.keyDown('space')
@@ -38,54 +50,51 @@ def jump_forward():
     last_input.append('space')
 
 def destroy():
-    pyautogui.click(button='left')
-
+    pyautogui.mouseDown(button='left')
 def place():
-    pyautogui.click(button='right')
-
-
-last_input = []
-recognizer = sr.Recognizer()
-
-
-timeout_counter = 0
-listening = False
-
-
+    pyautogui.mouseDown(button='right')
 
 def detect_gaze_movements():
     while True:
         if gt.gaze.is_right() :
-            pyautogui.move(1, 0)
+            pyautogui.move(40, 0)
         
         elif gt.gaze.is_left() : 
-            pyautogui.move(-1, 0)
+            pyautogui.move(-40, 0)
         
         elif gt.gaze.is_top() :
-            pyautogui.move(0, -1)
+            pyautogui.move(0, -40)
         
         elif gt.gaze.is_bottom() :
-            pyautogui.move(0, 1)
-
+            pyautogui.move(0, 40)
 
 
 def take_command():
-    while True:
-        with sr.Microphone() as source:
-            print('listening...')
+    print("Entering take_command")
+    with sr.Microphone() as source:
+        print('Microphone is open...')
+        recognizer.adjust_for_ambient_noise(source)
+        print('Listening...')
+        try:
             voice = recognizer.listen(source)
+            print('Processing voice...')
             command = recognizer.recognize_google(voice)
             command = command.lower()
             if 'command' in command:
                 command = command.replace('command', '')
-                print(command)
+                print(f"Recognized command: {command}")
             return command
-
+        except sr.UnknownValueError:
+            print("Sorry, I did not understand that.")
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+        return ""
 
 def run_microphone():
-    while True:        
+    print("Microphone thread started")
+    while True:
         command = take_command()
-        print(command)
+        print(f"Command received: {command}")
         if "forward" in command:
             go_forward()
         elif "backward" in command:  
@@ -94,8 +103,8 @@ def run_microphone():
             go_left()
         elif "right" in command:
             go_right()
-        elif "slot one" in command:
-            slot_1()
+        elif "slot three" in command:
+            slot_3()
         elif "jump" in command:
             jump()
         elif "break" in command:
@@ -109,16 +118,21 @@ def run_microphone():
         else:
             print('Please say the command again.')
 
-
-
 # Création des threads
 mic_thread = threading.Thread(target=run_microphone)
 head_movement_thread = threading.Thread(target=detect_gaze_movements)
 
 # Démarrage des threads
+print("Starting threads")
 mic_thread.start()
 head_movement_thread.start()
+
+# Ajout de pauses pour s'assurer que les threads démarrent correctement
+import time
+time.sleep(1)
 
 # Attente de la fin des threads (ce blocage du programme permet de garder les threads actifs)
 mic_thread.join()
 head_movement_thread.join()
+
+print("Threads have been started")
